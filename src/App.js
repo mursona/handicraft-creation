5c759ea9f4c04df6b2241f388f483d2d7fc81d02
mursona <mursona>
2022-09-29 15:38:50 +0600

    question section add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Designcard from './components/Designcard/Designcard';
 import Header from './components/Header/Header';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Designcard></Designcard>
+      <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Question.css';
+
+const Question = () => {
+    return (
+        <div>
+
+<div class="row1-container">
+          <div class="box box-down cyan">
+          <h3></h3>
+            <p>
+            </p>
+          </div>
+      
+          <div class="box red">
+          <h3></h3>
+            <p>
+            </p>
+          </div>
+      
+          <div class="box box-down blue">
+            <h3></h3>
+            <p>
+            </p>
+          </div>
+        </div>
+        </div>
+    );
+};
+
+export default Question;
