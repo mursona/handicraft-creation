3e5cc906d5ac0e35beaf84547db6dc3685b65ef3
mursona <mursona>
2022-09-29 18:48:46 +0600

    question and answer section

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,25 +4,35 @@ import './Question.css';
 const Question = () => {
     return (
         <div>
-
-<div class="row1-container">
-          <div class="box box-down cyan">
-          <h4>he</h4>
-            <p>
-            </p>
-          </div>
-      
-          <div class="box box-down red">
-          <h4>po</h4>
-            <p>
-            </p>
-          </div>
-      
-          <div class="box box-down blue">
-            <h4>lk</h4>
-            <p>
-            </p>
-          </div>
+            <div>
+            <h2>Top 3 ReactJS Interview Questions and Answers</h2>
+            <hr/>
+            </div>
+            <div className='row-container'>
+            <div className='col1'>
+            <h4>How Does React Works?</h4>
+                    <p>
+                        React defines the logical structure of documents and the way a document is accessed and manipulated. So, to make it faster, React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser's DOM.
+                    </p>
+            </div>
+                
+            <div className='col2'>
+            <h4>Difference between props and state in React.</h4>
+                <p>
+                    <ul>
+                        <li>Components receive data from outside with props, whereas they can create and manage their own data with state</li>
+                        <li>Props are used to pass data, whereas state is for managing data</li>
+                        <li>Data from props is read-only, and cannot be modified by a component that is receiving it from outside</li>
+                    </ul>
+                </p>
+            </div>
+                
+            <div className='col3'>
+            <h4>Where use useEffect except data fetching?</h4>
+                <p>
+                    useEffect after render: We know that, the useEffect() is used for causing side effects in functional components and it is also capable for handling componentDidMount(), componentDidUpdate() and componentWillUnmount() life-cycle methods of class based components into functional component.
+                </p>
+            </div>
         </div>
         </div>
     );
